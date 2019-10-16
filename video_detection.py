429c0404ce271ba0d8d28a417ed37bdcaaf32d9c
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-16 16:53:08 +0300

    Запись по фрейму в цветах RGB

--- video_detection.py
+++ video_detection.py
@@ -1,7 +1,18 @@
+import cv2 as cv
 from imageai.Detection import VideoObjectDetection
 import os
 
 execution_path = os.getcwd()
+
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('output.avi', fourcc, 29.97, (1280, 720))
+
+
+def forFrame(frame_number, output_array, output_count, detected_frame):
+    frame = detected_frame
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    out.write(frame)
+
 
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
@@ -10,8 +21,12 @@ detector.loadModel()
 
 video_path = detector.detectObjectsFromVideo(
     input_file_path=os.path.join(execution_path, "piter2.mp4"),
-    output_file_path=os.path.join(execution_path, "piter2_detected_1"),
+    save_detected_video=False,
+    # output_file_path=os.path.join(execution_path, "piter2_detected_1"),
     frames_per_second=29.97,
-    log_progress=True
+    log_progress=True,
+    per_frame_function=forFrame,
+    return_detected_frame=True
 )
+out.release()
 print(video_path)
